d65b46cd31f98a1583c44984d23610b2cc9c1217
livehass <silva.felipe12@hotmail.com>
2023-05-03 17:08:41 -0300

    new-commit-2

--- js/main.js
+++ js/main.js
@@ -1,36 +1,41 @@
-const form = document.getElementById("novoItem");
-const lista = document.getElementById("lista");
-const itens = JSON.parse(localStorage.getItem("itens")) || []
+const form = document.getElementById("novoItem") 
+const lista = document.getElementById("lista")
+const itens = JSON.parse(localStorage.getItem("itens")) || []  
 
-
-itens.forEach((elemento)=>{
+itens.forEach( (elemento) => {    
     criaElemento(elemento)
-});
+} )
 
-form.addEventListener("submit", (evento)=>{
+form.addEventListener("submit", (evento) => {  
     evento.preventDefault()
-    const nome = evento.target['nome'];
-    const quantidade = evento.target['quantidade'];
-    
 
-    
+    const nome = evento.target.elements['nome']
+    const quantidade = evento.target.elements['quantidade']
 
     const itemAtual = {
-        "nome": nome.value,
-        "quantidade": quantidade.value,
+    "nome": nome.value,
+    "quantidade": quantidade.value
     }
-    
-    quantidade.value ="";
-});
-function criaElemento(item){
-    const novoItem = document.createElement('li'); 
-    novoItem.classList.add("item");
-    const numeroItem = document.createElement('strong');
-    numeroItem.innerHTML = item.quantidade;
-    novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += item.nome;
-    lista.appendChild(novoItem);
-    
 
+    criaElemento(itemAtual)
+
+    itens.push(itemAtual)
+
+    localStorage.setItem("itens", JSON.stringify(itens))
+
+    nome.value = ""
+    quantidade.value = ""
+})
+
+function criaElemento(item) {  
+    const novoItem = document.createElement('li')
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement('strong')
+    numeroItem.innerHTML = item.quantidade
+    novoItem.appendChild(numeroItem)
+
+    novoItem.innerHTML += item.nome
+
+    lista.appendChild(novoItem)
 }
-
